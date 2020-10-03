2dd7d495bc9ba250f27aea8507090f3d68367aba
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-03 13:15:22 +0200

    add setTimeout

--- js/main.js
+++ js/main.js
@@ -1,62 +1,74 @@
-// // Initialisation des variables
-
-// const carreRouge = document.querySelector('.carreRouge');
-// const carreBleu = document.querySelector('.carreBleu');
-// const carreJaune = document.querySelector('.carreJaune');
-// const carreVert = document.querySelector('.carreVert');
-// const sequence = [];
-
-// // Création des fonctions
-
-// function randomInteger(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// function addOpacity(square) {
-//   square.style.opacity = '0.7';
-// }
-
-// function removeOpacity(square) {
-//   square.style.opacity = '1';
-// }
-
-// // Fonction principale
-
-// function scaleSquare() {
-//   const currentRandomInteger = randomInteger(1, 4);
-
-//   if (currentRandomInteger === 1) {
-//     addOpacity(carreRouge);
-//     // removeOpacity(carreRouge);
-//   } else if (currentRandomInteger === 2) {
-//     addOpacity(carreBleu);
-//     // removeOpacity(carreBleu);
-//   } else if (currentRandomInteger === 3) {
-//     addOpacity(carreJaune);
-//     // removeOpacity(carreJaune);
-//   } else if (currentRandomInteger === 4) {
-//     addOpacity(carreVert);
-//     // removeOpacity(carreVert);
-//   } else null;
-
-//   sequence.push(currentRandomInteger);
-// }
-
-// // Lancement du jeu
-
-// scaleSquare();
+// Initialisation des variables
 
 const carreRouge = document.querySelector('.carreRouge');
+const carreBleu = document.querySelector('.carreBleu');
+const carreJaune = document.querySelector('.carreJaune');
+const carreVert = document.querySelector('.carreVert');
+const sequence = [];
 
-console.log(carreRouge);
+// Création des fonctions
 
-function modifyOpacity() {
-  carreRouge.style.opacity = '0.7';
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function removeOpacity() {
-  carreRouge.style.opacity = '1';
+function addOpacity(square) {
+  square.style.opacity = '0.7';
 }
 
-modifyOpacity();
-setTimeout(removeOpacity, 2000);
+function removeOpacity(square) {
+  square.style.opacity = '1';
+} 
+
+function
+
+// Fonction principale
+
+function main() {
+  const currentRandomInteger = randomInteger(1, 4);
+  console.log(currentRandomInteger);
+
+  if (currentRandomInteger === 1) {
+    addOpacity(carreRouge);
+    setTimeout(function () {
+      removeOpacity(carreRouge);
+    }, 2000);
+  } else if (currentRandomInteger === 2) {
+    addOpacity(carreBleu);
+    setTimeout(function () {
+      removeOpacity(carreBleu);
+    }, 2000);
+  } else if (currentRandomInteger === 3) {
+    addOpacity(carreJaune);
+    setTimeout(function () {
+      removeOpacity(carreJaune);
+    }, 2000);
+  } else if (currentRandomInteger === 4) {
+    addOpacity(carreVert);
+    setTimeout(function () {
+      removeOpacity(carreVert);
+    }, 2000);
+  } else null;
+
+  sequence.push(currentRandomInteger);
+}
+
+// Lancement du jeu
+
+setTimeout(main, 5000);
+
+
+// const carreRouge = document.querySelector('.carreRouge');
+
+// console.log(carreRouge);
+
+// function modifyOpacity() {
+//   carreRouge.style.opacity = '0.7';
+// }
+
+// function removeOpacity() {
+//   carreRouge.style.opacity = '1';
+// }
+
+// modifyOpacity();
+// setTimeout(removeOpacity, 2000);
