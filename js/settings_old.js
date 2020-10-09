6217fc4e4eef45803d0059cba368b912ec4eccee
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-09 15:20:02 +0200

    rename file

--- js/settings_old.js
+++
@@ -1,9 +0,0 @@
-// const login = document.querySelector('.current-login');
-// const currentLogin = login.innerHTML;
-
-// if (currentLogin === 'anonymous') {
-//   const promptLogin = prompt('Enter your name');
-//   login.innerText = promptLogin;
-// } else {
-//   null;
-// }
